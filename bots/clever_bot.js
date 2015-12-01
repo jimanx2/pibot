6f7e3fa14f9b58c8e4009c34a7ed53be5de53dfb
Haziman Hashim <haziman@abh.my>
2015-12-01 11:38:58 +0800

    fix cleverbot

--- bots/clever_bot.js
+++ bots/clever_bot.js
@@ -13,7 +13,7 @@ module.exports = function(){
     function startListen(params, msg){
       Cleverbot.prepare(function(){
         convId = msg.chat.id;
-        bot.sendMessage(convId, "Yes sir?");
+        bot.sendMessage(convId, "Yes sir?", { reply_markup: { force_reply: true } });
         $listening = true;
       });
     };
@@ -31,6 +31,38 @@ module.exports = function(){
     this.$desc["done"] = "- Stop the AI";
     
     bot.on($this.$name, function(params, msg){
+      if($listening){
+        console.log("CleverBot :", params.join(' '));
+
+        // not the required chat
+        if( !convId ){
+          return;
+        }
+        if( msg.chat.id != convId ){
+          console.log(update.message.chat.id,"!=", convId);
+          return;
+        }
+
+        var phrase = params.join(' ');
+        // console.log("Sending ",phrase );
+        if($asking)
+          return bot.sendMessage(convId, "Please wait. I'm thinking.", {
+            reply_to_message_id: msg.message_id,
+            reply_markup: { force_reply: true }
+          });
+
+        $asking = true;  
+        cleverbot.write(phrase, function (response) {
+          // console.log("Got ",response );
+          bot.sendMessage(convId, response.message, {
+            reply_to_message_id: msg.message_id,
+            reply_markup: { force_reply: true }
+          });
+          $asking = false;
+        });
+        return;
+      }
+      
       if(params.length == 0)
         return $this.$tasks["desc"]([], msg);
 
@@ -44,39 +76,6 @@ module.exports = function(){
       $this.$tasks[task](params, msg);
     });
     
-    bot.on('update', function(update) {
-      console.log("CleverBot :", update.message.text);
-      
-      if( update.message.text.trim()[0] == "/" ) return;
-      var params = update.message.text.split(" ");
-      var keyword = params.shift();
-      
-      // not the required chat
-      if( !convId ){
-        return;
-      }
-      if( update.message.chat.id != convId ){
-        console.log(update.message.chat.id,"!=", convId);
-        return;
-      }
-
-      params.unshift(keyword);
-      var phrase = params.join(' ');
-      // console.log("Sending ",phrase );
-      if($asking)
-        return bot.sendMessage(convId, "Please wait. I'm thinking.", {
-          reply_to_message_id: update.message.message_id
-        });
-
-      $asking = true;  
-      cleverbot.write(phrase, function (response) {
-        // console.log("Got ",response );
-        bot.sendMessage(convId, response.message, {
-          reply_to_message_id: update.message.message_id
-        });
-        $asking = false;
-      });
-    });
   }
   CleverBot.prototype = new Bot();
   CleverBot.prototype.constructor = CleverBot;
