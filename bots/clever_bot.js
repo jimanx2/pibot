3f51c5d2ab7e4f911c0379b45a82fb37666f585b
Haziman Hashim <haziman@abh.my>
2015-12-03 10:43:45 +0800

    update cleverbot

--- bots/clever_bot.js
+++ bots/clever_bot.js
@@ -21,12 +21,12 @@ module.exports = function(){
     ];
     var conversations = {};
     var Cleverbot = require("cleverbot.io");
+    var cleverbot = cleverbot = new Cleverbot("JQU5usm68qkXWHKQ", "g0giw78pG84AUw684798zYDWVMSu1FRj");
     
     function startListen(params, msg){
       if(conversations[msg.chat.id])
         return bot.sendMessage(msg.chat.id, "Already started");
         
-      var cleverbot = new Cleverbot("JQU5usm68qkXWHKQ", "g0giw78pG84AUw684798zYDWVMSu1FRj");
       cleverbot.create(function (err, nick) {
         conversations[msg.chat.id] = { 
           bot: cleverbot, ref: nick, $asking: false, $listening: true,
